a3afe9b813a3aab9fa199ecd59d7f8e506408011
cthebest <119713971+cthebest@users.noreply.github.com>
2023-01-04 15:33:06 -0500

    overflow table

--- resources/js/components/atoms/table/Table.svelte
+++ resources/js/components/atoms/table/Table.svelte
@@ -1,4 +1,4 @@
-<div class="bg-slate-50 border rounded">
+<div class="bg-slate-50 border rounded overflow-x-auto">
     <table class="border-collapse table-auto w-full text-sm">
         <slot></slot>
     </table>
